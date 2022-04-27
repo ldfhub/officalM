ab94e6481a9e75d3b816d4a9ee67cce149bacaf3
lidengfeng <coderldf580@163.com>
2022-04-27 14:59:35 +0800

    ci: 修复路由初始化错误

--- src/layouts/ footer/index.tsx
+++ src/layouts/ footer/index.tsx
@@ -1,44 +1,62 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import styles from './index.less';
 import { useHistory } from 'react-router-dom';
 
 const FooterNav = () => {
   const history = useHistory();
   const nav = [
-    {text: '首页', key: 'home', active: true},
-    {text: '列表', key: 'list', active: false},
-    {text: '上传', key: 'upload', active: false},
-    {text: '我的', key: 'profile', active: false}
-  ]
+    { text: '首页', key: 'home', active: true },
+    { text: '列表', key: 'list', active: false },
+    { text: '上传', key: 'upload', active: false },
+    { text: '我的', key: 'profile', active: false },
+  ];
+  console.log(history);
   const [navData, setNavData] = useState(nav);
+
+  useEffect(() => {
+    initRouter();
+  }, []);
+
+  const pathName: string = history.location.pathname;
+  const initRouter = () => {
+    const num = navData.findIndex((item) => '/' + item.key === pathName);
+    navData.forEach((item, index) => {
+      item.active = false;
+      navData[num].active = true;
+    });
+    setNavData([...navData]);
+  };
   // 点击切换路由
   const clickChangeRouter = (key: string) => {
     return () => {
-      const num = navData.findIndex(item => item.key === key);
+      const num = navData.findIndex((item) => item.key === key);
       navData.forEach((item, index) => {
         item.active = false;
         navData[num].active = true;
-      })
+      });
       setNavData([...navData]);
-      history.push(`/${key}`)
-    }
-  }
+      history.push(`/${key}`);
+    };
+  };
   return (
     <div className={styles.footerNav}>
       <ul>
-        {
-          navData.map((item, index) => {
-            return (
-              <li key={item.key} onClick={clickChangeRouter(item.key)}>
-                <img src={require(`../../assets/${item.active ? item.key + '-active' : item.key}.png`)} />
-                <span className={item.active ? styles.activeText : ''}>{item.text}</span>
-              </li>
-            )
-          })
-        }
+        {navData.map((item, index) => {
+          return (
+            <li key={item.key} onClick={clickChangeRouter(item.key)}>
+              <img
+                src={require(`../../assets/${
+                  item.active ? item.key + '-active' : item.key
+                }.png`)}
+              />
+              <span className={item.active ? styles.activeText : ''}>
+                {item.text}
+              </span>
+            </li>
+          );
+        })}
       </ul>
     </div>
-  )
-}
+  );
+};
 export default FooterNav;
-
